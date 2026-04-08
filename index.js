e3c613aee707e9f3ad2c82f071c4fe96dd60dd06
tekcantekvatan-ctrl <tek.can.tek.vatan@gmail.com>
2026-04-08 20:50:30 +0300

    index.js

--- index.js
+++ index.js
@@ -8,18 +8,28 @@ const client = new Client({
 });
 
 const TOKEN = process.env.TOKEN;
-const ROLE_ID = "1128692866567381025";
+const ROLE_ID = "1491485420100521994";
+const CHANNEL_ID = "1422509177481199669"; // 👈 bunu ekle
 
 client.on("clientReady", () => {
   console.log(`${client.user.tag} aktif!`);
 });
 
 client.on("guildMemberAdd", async (member) => {
+
+  // 🎉 HOŞ GELDİN MESAJI
+  const channel = member.guild.channels.cache.get(CHANNEL_ID);
+  if (channel) {
+    channel.send(`🎉 ${member.user} sunucuya katıldı!`);
+  }
+
+  // 🎭 ROL VERME
   const role = member.guild.roles.cache.get(ROLE_ID);
   if (role) {
     await member.roles.add(role);
     console.log("Rol verildi");
   }
+
 });
 
 client.login(TOKEN);
