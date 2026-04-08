f22b34243020e3386dd42b4cd646274dd4f589bf
tekcantekvatan-ctrl <tek.can.tek.vatan@gmail.com>
2026-04-08 21:01:37 +0300

    index.js

--- index.js
+++ index.js
@@ -9,25 +9,45 @@ const client = new Client({
 
 const TOKEN = process.env.TOKEN;
 const ROLE_ID = "1128692866567381025";
-const CHANNEL_ID = "1422509177481199669"; // 👈 bunu ekle
+
+const JOIN_CHANNEL_ID = "1422509177481199669";
+const LEAVE_CHANNEL_ID = "1491496841437773974";
 
 client.on("clientReady", () => {
   console.log(`${client.user.tag} aktif!`);
 });
 
+// 👋 SUNUCUYA GİRİNCE
 client.on("guildMemberAdd", async (member) => {
 
-  // 🎉 HOŞ GELDİN MESAJI
-  const channel = member.guild.channels.cache.get(CHANNEL_ID);
+  const channel = member.guild.channels.cache.get(JOIN_CHANNEL_ID);
+
+  // 🎉 giriş mesajı
   if (channel) {
     channel.send(`🎉 ${member.user} sunucuya katıldı!`);
   }
 
-  // 🎭 ROL VERME
+  // 🎭 rol verme
   const role = member.guild.roles.cache.get(ROLE_ID);
+
   if (role) {
-    await member.roles.add(role);
-    console.log("Rol verildi");
+    try {
+      await member.roles.add(role);
+      console.log("Rol verildi");
+    } catch (err) {
+      console.log("Rol verilemedi:", err.message);
+    }
+  }
+
+});
+
+// ❌ SUNUCUDAN ÇIKINCA
+client.on("guildMemberRemove", (member) => {
+
+  const channel = member.guild.channels.cache.get(LEAVE_CHANNEL_ID);
+
+  if (channel) {
+    channel.send(`❌ ${member.user.tag} sunucudan ayrıldı!`);
   }
 
 });
