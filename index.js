c21cdbfd3554dcaf4f7af0a2dd6c04ce6fcd80b5
tekcantekvatan-ctrl <tek.can.tek.vatan@gmail.com>
2026-04-08 20:53:07 +0300

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Client({
 });
 
 const TOKEN = process.env.TOKEN;
-const ROLE_ID = "1491485420100521994";
+const ROLE_ID = "1128692866567381025";
 const CHANNEL_ID = "1422509177481199669"; // 👈 bunu ekle
 
 client.on("clientReady", () => {
